20bad97de53315935305b73e7adcfdde038a7a6f
LinkolnR <lincolninsper@gmail.com>
2022-07-03 12:38:44 -0300

    mais testes adicionados

--- specs/units/service/LoremIpsum.spec.ts
+++ specs/units/service/LoremIpsum.spec.ts
@@ -9,22 +9,39 @@ describe('Teste do Lorem Ipsum', () => {
       expect(_service).toBeTruthy()
     })
 
-    it('Deve uma string com uma (1) palavra, aqui vamos dar split e olhar para o tamanho', () => {
+    it('Deve retornar uma string com uma (1) palavra, aqui vamos dar split e olhar para o tamanho', () => {
       const resultado  = _service.getLoremIpsum(1)
       let tamanho = resultado.split(" ").length
       expect(tamanho).toEqual(1)
     })
 
-    it('Deve uma string com duas (2) palavra, aqui vamos dar split e olhar para o tamanho', () => {
+    it('Deve retornar uma string com duas (2) palavra, aqui vamos dar split e olhar para o tamanho', () => {
       const resultado = _service.getLoremIpsum(2)
       let tamanho = resultado.split(" ").length
       expect(tamanho).toEqual(2)
     }) 
 
-    it('Deve uma string com duas (3) palavra, aqui vamos dar split e olhar para o tamanho', () => {
+    it('Deve retornar uma string com duas (3) palavra, aqui vamos dar split e olhar para o tamanho', () => {
       const resultado = _service.getLoremIpsum(3)
       let tamanho = resultado.split(" ").length
       expect(tamanho).toEqual(3)
     })
+
+    it('Deve retornar uma string com o conteúdo \'invalido\'', () => {
+      const resultado = _service.getLoremIpsum(0)
+      
+      expect(resultado).toEqual("invalido")
+    })
+
+    it('Deve retornar uma string com o conteúdo \'invalido\' ', () => {
+      const resultado = _service.getLoremIpsum(-10)
+      
+      expect(resultado).toEqual("invalido")
+    })
   
+    it('Deve retornar uma string com cem(100) palavra, aqui vamos dar split e olhar para o tamanho', () => {
+      const resultado  = _service.getLoremIpsum(100)
+      let tamanho = resultado.split(" ").length
+      expect(tamanho).toEqual(100)
+    })
   })
