05e7f38e861313cb068309b2b965beaffec3262f
LinkolnR <lincolninsper@gmail.com>
2022-07-04 22:17:35 -0300

    Verificador está funcionando

--- specs/units/service/cpf.spec.ts
+++ specs/units/service/cpf.spec.ts
@@ -1,18 +1,29 @@
-
+import { CpfService } from "../../../src/services/CpfService"
 
 describe('Teste do CPF', () => {
+    let _service : CpfService
+    it ('deveria instanciar corretamente a classe', () => {
+        _service = new CpfService()
+    
+        expect(_service).toBeTruthy()
+      })
 
-    it('Deve retornar um cpf válido', () => {
-        const resultado  = _service.geraCpf()
-        let tamanho = resultado.split("-").length
-        expect(tamanho).toEqual(1)
-    })
+    // it('Deve retornar um cpf válido', () => {
+    //     const resultado  = _service.geraCpf()
+    //     let tamanho = resultado.length
+    //     expect(tamanho).toEqual(1)
+    // })
 
-    it('Deve verificar se um cpf é válido', () => {
+    it('Deve verificar se um cpf é válido, teste para um válido retorna true', () => {
         let cpf = "000000000-00"
         const resultado = _service.validaCpf(cpf)
-        let tamanho = resultado.split(" ").length
-        expect(tamanho).toEqual(2)
+        expect(resultado).toBe(true)
+    }) 
+
+    it('Deve verificar se um cpf é válido, teste para um inválido retorna false', () => {
+        let cpf = "000000000-10"
+        const resultado = _service.validaCpf(cpf)
+        expect(resultado).toBe(false)
     }) 
 
   })
---
+++ src/services/CpfService.ts
@@ -0,0 +1,45 @@
+export class CpfService {
+
+    geraCpf = () =>{
+        var cpf 
+        cpf = ""
+        while (cpf.length<9){
+            var aleatorio = Math.floor(Math.random() * 9);
+            cpf+= aleatorio
+        } 
+        let j = 1
+        let dv = 0
+        for (let index = 0; index < 9; index++) {
+            if (j ==4){
+                j = 1
+            }
+            {
+                let numero  = parseInt(cpf.slice(index,index+1),10)
+                dv+= numero*j
+                j+=1
+            }
+        }
+        cpf += "-" + dv
+        return cpf
+    }
+
+    validaCpf = (cpf: string) =>{
+        let j = 0 
+        let dv = 0
+        for (let index = 0; index < 9; index++) {
+            if (j ==4){
+                j = 1
+            }
+            {
+                let numero  = parseInt(cpf.slice(index,index+1),10)
+                dv+= numero*j
+                j+=1
+            }
+        }
+        let final = parseInt(cpf.slice(10,12),10)
+        if (final == dv){
+            return true
+        }
+        return false
+    }
+}
